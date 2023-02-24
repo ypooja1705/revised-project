3683be3e651b125ae068d6b849baab456e372403
shashi <shashiyadav1705@gmail.com>
2023-02-24 12:13:28 +0530

    marketing component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { Supplier2Component } from './supplier2/supplier2.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
 import Comp3Component from './comp3/comp3.component';
+import { MarketingComponent } from './marketing/marketing.component';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import Comp3Component from './comp3/comp3.component';
     Comp1Component,
     Comp2Component,
     Comp3Component,
+    MarketingComponent,
   
     
   ],
