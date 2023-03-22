eff3021755ac6d0e60f89c22f4ceb8c3e751e029
shashi <shashiyadav1705@gmail.com>
2023-03-22 13:16:57 +0530

    comment added

--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -5,8 +5,9 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class serviceservice {
-  constructor(private http:HttpClient) { 
+  constructor(private http:HttpClient) { //to create a object of http we will use dependency injection  
   }
+  //once the http object  created now we can make post and get call
   getCustomers(){
     return this.http.get("http://localhost:3000/Customer");
    }
